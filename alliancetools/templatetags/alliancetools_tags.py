5c24447d5c05b9456cd80e324d13bc2b872428ff
Aaron Kable <aaronkable@gmail.com>
2019-09-20 13:47:38 +0000

    moon timer

--- alliancetools/templatetags/alliancetools_tags.py
+++ alliancetools/templatetags/alliancetools_tags.py
@@ -106,3 +106,24 @@ def evename_img(_id, name, cat, size):
     else:
         return ""
 
+
+@register.filter()
+def ore_color(ore):
+    try:
+        if 'Bitumens' in ore or 'Coesite' in ore or 'Sylvite' in ore or 'Zeolite' in ore:
+            return mark_safe('style="background-color:#9B1C31"')
+        elif 'Cobalite' in ore or 'Euxenite' in ore or 'Scheelite' in ore or 'Titanite' in ore:
+            return mark_safe('style="background-color:#E86100"')
+        elif 'Chromite' in ore or 'Otavite' in ore or 'Sperrylite' in ore or 'Vanadinite' in ore:
+            return mark_safe('style="background-color:#FFAA1D"')
+        elif 'Carnotite' in ore or 'Cinnabar' in ore or 'Pollucite' in ore or 'Zircon' in ore:
+            return mark_safe('style="background-color:#4B8B3B"')
+        elif 'Loparite' in ore or 'Monazite' in ore or 'Xenotime' in ore or 'Ytterbite' in ore:
+            return mark_safe('style="background-color:#0D98BA"')
+        else:
+            return mark_safe('style="background-color:#B57EDC"')
+
+    except:
+        #logging.exception("Messsage")
+        return ""
+
